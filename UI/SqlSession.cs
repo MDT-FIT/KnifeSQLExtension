787e6f7e452ee10df6de71a5e80ddd79a3148b1b
Maria Stashenko <mahryia@gmail.com>
2026-03-12 19:17:10 +0200

    update: main gui class by separating connection view and adding shared session object

---
+++ UI/SqlSession.cs
@@ -0,0 +1,15 @@
+﻿using KnifeSQLExtension.Core.Services.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnifeSQLExtension.UI
+{
+    public class SqlSession
+    {
+        public IDatabaseClient? DbClient { get; set; } = null;
+        public bool IsConnected { get; set; }
+    }
+}
