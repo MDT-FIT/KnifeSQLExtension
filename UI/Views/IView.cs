316e4290719a830da56b63fe3b99039424ca4d5e
Maria Stashenko <mahryia@gmail.com>
2026-03-13 16:20:43 +0200

    add: interface for views to expose View property

---
+++ UI/Views/IView.cs
@@ -0,0 +1,14 @@
+﻿using DevToys.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnifeSQLExtension.UI.Views
+{
+    public interface IView
+    {
+        IUIElement View { get; }
+    }
+}
