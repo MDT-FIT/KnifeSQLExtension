e5a0c8d865c4bb5d0642a4333d6917f74238e194
Maria Stashenko <mahryia@gmail.com>
2026-03-15 14:01:19 +0200

    fix: filter circular and/or nullable dependencies

--- Features/RandomDataGeneration/Graph.cs
+++ Features/RandomDataGeneration/Graph.cs
@@ -1,10 +1,5 @@
 ﻿using KnifeSQLExtension.Core.Models;
-using SqlParser.Ast;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace KnifeSQLExtension.Features.RandomDataGeneration
 {
@@ -23,7 +18,41 @@ namespace KnifeSQLExtension.Features.RandomDataGeneration
 
         private List<string> GetReferencedTables(TableSchema table)
         {
-            return table.Columns.Where(c => c.FkObject is not null).Select(c => c.FkObject.FkTableName).ToList();
+            return table.Columns
+                .Where(c => c.FkObject is not null && !c.IsNullable) // only hard dependencies
+                .Select(c => c.FkObject.FkTableName)
+                .ToList();
+        }
+
+        public List<string> GetSortedTables()
+        {
+            Dictionary<string, int> indegree = [];
+            List<string> sortedTables = [];
+            Queue<string> queue = [];
+
+            // Initialize dependencies count for each table
+            foreach(var item in Value)
+                indegree[item.Key] = item.Value.Count;
+
+            // Enque tables without dependencies first
+            foreach(var item in indegree)
+                if(indegree[item.Key] == 0)
+                    queue.Enqueue(item.Key);
+
+            // Resolve dependencies in order from least to most dependent
+            while(queue.Count > 0)
+            {
+                string top = queue.Dequeue();
+                sortedTables.Add(top);
+                foreach(string next in Value[top])
+                {
+                    indegree[next]--;
+                    if(indegree[next] == 0)
+                        queue.Enqueue(next);
+                }
+            }
+
+            return sortedTables;
         }
     }
 }
