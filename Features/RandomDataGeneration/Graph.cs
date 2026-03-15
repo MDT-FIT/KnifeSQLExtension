5737cecf58c283dacafcf7f9cf6df76f33e372a9
Maria Stashenko <mahryia@gmail.com>
2026-03-15 13:50:57 +0200

    add: topological sort for tables based on their dependencies

--- Features/RandomDataGeneration/Graph.cs
+++ Features/RandomDataGeneration/Graph.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KnifeSQLExtension.Core.Models;
+using SqlParser.Ast;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace KnifeSQLExtension.Features.RandomDataGeneration
 {
-    internal class Graph
+    public class Graph
     {
+        public Dictionary<string, List<string>> Value { get; }
+
+        public Graph(List<TableSchema> tableSchemas)
+        {
+            Value = new Dictionary<string, List<string>>();
+            foreach(var table in tableSchemas) 
+            {
+                Value[table.Name] = GetReferencedTables(table);
+            }
+        }
+
+        private List<string> GetReferencedTables(TableSchema table)
+        {
+            return table.Columns.Where(c => c.FkObject is not null).Select(c => c.FkObject.FkTableName).ToList();
+        }
     }
 }
